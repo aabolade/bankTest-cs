88d2355236d8f9920a7cd412ea6927fec0804ae7
Abolade <ABOLADLX@legal.regn.net>
2017-05-25 15:34:44 +0100

    creating account class with balance and deposit method

---
+++ BankTest/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTest
+{
+    public class Account
+    {
+        public int balance;
+
+        public void deposit(int amount)
+        {
+            balance += amount;
+        }
+    }
+}
---
+++ BankTestTests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankTest;
+
+namespace BankTestTests
+{
+    [TestClass]
+    public class AccountTests
+    {
+        [TestMethod]
+        public void AccountShouldHaveABalance()
+        {
+            Account account = new Account();
+            Assert.AreEqual(account.balance, 0);
+        }
+
+        [TestMethod]
+        public void MakingADepositIncreasesTheAccountBalance()
+        {
+            Account account = new Account();
+            account.deposit(10);
+            Assert.AreEqual(account.balance, 10);
+        }
+
+
+    }
+}
